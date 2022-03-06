838021281952ae27f7ff8ae3e6f1cc5aa139e096
jianzhnie <jianzhnie@126.com>
2022-03-06 21:49:55 +0800

    Update model_gpt.py

--- nlptoolkit/models/transformer/gpt/model_gpt.py
+++ nlptoolkit/models/transformer/gpt/model_gpt.py
@@ -1,12 +1,20 @@
+import copy
 import logging
 
 import torch
 import torch.nn as nn
 from torch import Tensor
+from torch.nn import ModuleList
+from torch.nn import functional as F
 
-from nlptoolkit.models.attention.multihead_attn import Attention
+from nlptoolkit.models.attention.multihead_attn import (Attention,
+                                                        MultiHeadAttention)
 
 logger = logging.getLogger(__name__)
+
+
+def _get_clones(module, N):
+    return ModuleList([copy.deepcopy(module) for i in range(N)])
 
 
 class GPTConfig:
@@ -43,7 +51,6 @@ class CausalSelfAttention(nn.Module):
         self.key_linear = nn.Linear(d_model, d_model)
         self.value_linear = nn.Linear(d_model, d_model)
         # regularization
-        self.attn_dropout = nn.Dropout(dropout)
         self.resid_dropout = nn.Dropout(dropout)
         # output projection
         self.output_linear = nn.Linear(d_model, d_model)
@@ -88,12 +95,12 @@ class CausalSelfAttention(nn.Module):
         return output, self_attn
 
 
-class GPTBlock(nn.Module):
-    def __init__(self, d_model, n_head, dropout=0.1):
+class DecoderLayer(nn.Module):
+    def __init__(self, d_model, n_head, block_size, dropout=0.1):
         super().__init__()
         self.layer_norm1 = nn.LayerNorm(d_model)
         self.layer_norm2 = nn.LayerNorm(d_model)
-        self.attn = CausalSelfAttention(d_model, n_head, dropout=dropout)
+        self.attn = MultiHeadAttention(d_model, n_head, dropout=dropout)
         self.mlp = nn.Sequential(
             nn.Linear(d_model, 4 * d_model),
             nn.GELU(),
@@ -101,7 +108,7 @@ class GPTBlock(nn.Module):
             nn.Dropout(),
         )
 
-    def forward(self, src, src_mask):
+    def forward(self, src, src_mask=None):
         src2 = self.layer_norm1(src)
         src2 = self.attn(src2, src2, src2, mask=src_mask)[0]
         src = src + src2
@@ -110,3 +117,124 @@ class GPTBlock(nn.Module):
         src2 = self.mlp(src2)
         src = src + src2
         return src
+
+
+class GPTModel(nn.Module):
+    def __init__(self,
+                 vocab_size,
+                 d_model,
+                 n_head,
+                 num_layers,
+                 block_size,
+                 dropout=0.1):
+        super().__init__()
+
+        # input embedding stem
+        self.token_embedder = nn.Embedding(vocab_size, d_model)
+        self.pos_embedder = nn.Parameter(torch.zeros(1, block_size, d_model))
+        self.dropout = nn.Dropout(dropout)
+
+        # transformers
+        self.decoder_layer = DecoderLayer(d_model, n_head, dropout=dropout)
+        self.decoder = _get_clones(self.decoder_layer, num_layers)
+
+        # decoder head
+        self.layer_norm = nn.LayerNorm(d_model)
+        self.head = nn.Linear(d_model, vocab_size, bias=False)
+
+        self.block_size = block_size
+        self.apply(self._init_weights)
+
+    def get_block_size(self):
+        return self.block_size
+
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            module.weight.data.normal_(mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                module.bias.data.zero_()
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+
+    def forward(self, tokens, targets=None):
+        batch_size, seq_len = tokens.size()
+        assert seq_len <= self.block_size, 'Cannot forward, model block size is exhausted.'
+
+        # forward the GPT model
+        # each src maps to a (learnable) vector
+        token_embeddings = self.token_embedder(tokens)
+        position_embeddings = self.pos_embedder[:, :seq_len, :]
+        # each position maps to a (learnable) vector
+        x = self.dropout(token_embeddings + position_embeddings)
+        x = self.decoder(x)
+        x = self.layer_norm(x)
+        logits = self.head(x)
+
+        # if we are given some desired targets also calculate the loss
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)),
+                                   targets.view(-1))
+
+        return logits, loss
+
+
+def configure_optimizers(model, learning_rate, weight_decay, betas):
+    """This long function is unfortunately doing something very simple and is
+    being very defensive:
+
+    We are separating out all parameters of the model into two buckets: those that will experience weight decay for regularization and those that won't (biases,
+    and layernorm/embedding weights). We are then returning the PyTorch optimizer object.
+    """
+
+    # separate out all parameters to those that will and won't experience regularizing weight decay
+    decay = set()
+    no_decay = set()
+    whitelist_weight_modules = (torch.nn.Linear, )
+    blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+    for mn, m in model.named_modules():
+        for pn, p in m.named_parameters():
+            fpn = '%s.%s' % (mn, pn) if mn else pn  # full param name
+
+            if pn.endswith('bias'):
+                # all biases will not be decayed
+                no_decay.add(fpn)
+            elif pn.endswith('weight') and isinstance(
+                    m, whitelist_weight_modules):
+                # weights of whitelist modules will be weight decayed
+                decay.add(fpn)
+            elif pn.endswith('weight') and isinstance(
+                    m, blacklist_weight_modules):
+                # weights of blacklist modules will NOT be weight decayed
+                no_decay.add(fpn)
+
+    # special case the position embedding parameter in the root GPT module as not decayed
+    no_decay.add('pos_emb')
+
+    # validate that we considered every parameter
+    param_dict = {pn: p for pn, p in model.named_parameters()}
+    inter_params = decay & no_decay
+    union_params = decay | no_decay
+    assert len(
+        inter_params
+    ) == 0, 'parameters %s made it into both decay/no_decay sets!' % (
+        str(inter_params), )
+    assert len(
+        param_dict.keys() - union_params
+    ) == 0, 'parameters %s were not separated into either decay/no_decay set!' % (
+        str(param_dict.keys() - union_params), )
+
+    # create the pytorch optimizer object
+    optim_groups = [
+        {
+            'params': [param_dict[pn] for pn in sorted(list(decay))],
+            'weight_decay': weight_decay
+        },
+        {
+            'params': [param_dict[pn] for pn in sorted(list(no_decay))],
+            'weight_decay': 0.0
+        },
+    ]
+    optimizer = torch.optim.AdamW(optim_groups, lr=learning_rate, betas=betas)
+    return optimizer
