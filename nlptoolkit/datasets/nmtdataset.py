6545d518d59e6d88ab5c46d3344e03afb9f32cef
jianzhnie <jianzhnie@126.com>
2022-03-28 19:48:04 +0800

    Update nmtdataset.py

--- nlptoolkit/datasets/nmtdataset.py
+++ nlptoolkit/datasets/nmtdataset.py
@@ -1,9 +1,6 @@
-import os
 import sys
 
-import d2l
 import torch
-from torch.utils import data
 
 from nlptoolkit.data.vocab import Vocab
 
@@ -14,23 +11,22 @@ class NMTDatasets():
     """Defined in :numref:`sec_machine_translation`"""
     def __init__(self,
                  root='../data',
+                 batch_size=32,
                  num_steps=10,
                  num_train=1000,
-                 num_val=1000,
-                 download=True):
+                 num_val=1000):
         super(NMTDatasets, self).__init__()
         self.root = root
-        self.download = download
+        self.batch_size = batch_size
+        self.num_steps = num_steps
+        self.num_train = num_train
+        self.num_val = num_val
 
         self.arrays, self.src_vocab, self.tgt_vocab = self._build_arrays(
             self._read_data())
 
     def _read_data(self):
         """Load the English-French dataset."""
-        if self.download:
-            d2l.extract(
-                d2l.download(d2l.DATA_URL + 'fra-eng.zip', self.root,
-                             '94646ad1522d915e7b0f9296181140edcf86a4f5'))
         with open(self.root + '/fra-eng/fra.txt', encoding='utf-8') as f:
             return f.read()
 
@@ -70,33 +66,28 @@ class NMTDatasets():
             return line[:num_steps]  # Truncate
         return line + [padding_token] * (num_steps - len(line))  # Pad
 
+    def _bulid_array_nmt(self, tokens, vocab=None, num_steps=10):
+        """Transform text sequences of machine translation into minibatches."""
+        if vocab is None:
+            vocab = Vocab(tokens,
+                          min_freq=2,
+                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
+
+        text_tokens = [vocab[token] for token in tokens]
+        text_tokens = [token + [vocab['<eos>']] for token in text_tokens]
+        data_array = torch.tensor([
+            self.truncate_pad(l, num_steps, vocab['<pad>'])
+            for l in text_tokens
+        ])
+        return data_array, vocab
+
     def _build_arrays(self, raw_text, src_vocab=None, tgt_vocab=None):
         """Defined in :numref:`sec_machine_translation`"""
-        def _build_one(sentences, vocab):
-            sentences = [
-                self._truncate_pad(seq, self.num_steps, ['<pad>'])
-                for seq in sentences
-            ]
-            if vocab is None:
-                vocab = Vocab(sentences, min_freq=2)
-            array = d2l.tensor([vocab[sent] for sent in sentences])
-            return array, vocab
-
         src, tgt = self._tokenize(self._preprocess(raw_text),
                                   self.num_train + self.num_val)
-        src_array, src_vocab = _build_one(src, src_vocab)
-        tgt_array, tgt_vocab = _build_one(tgt, tgt_vocab)
-        return (src_array, tgt_array[:, :-1],
-                tgt_array[:, 1:]), src_vocab, tgt_vocab
-
-    def build(self, src_sentences, tgt_sentences):
-        """Defined in :numref:`sec_machine_translation`"""
-        raw_text = '\n'.join([
-            src + '\t' + tgt for src, tgt in zip(src_sentences, tgt_sentences)
-        ])
-        arrays, _, _ = self._build_arrays(raw_text, self.src_vocab,
-                                          self.tgt_vocab)
-        return arrays
+        src_array, src_vocab = self._bulid_array_nmt(src, src_vocab)
+        tgt_array, tgt_vocab = self._bulid_array_nmt(tgt, tgt_vocab)
+        return (src_array, tgt_array), src_vocab, tgt_vocab
 
     def get_dataloader(self, train):
         """Defined in :numref:`sec_machine_translation`"""
@@ -119,107 +110,17 @@ class NMTDatasets():
                                            shuffle=train)
 
 
-def read_data_nmt():
-    """Load the English-French dataset.
-
-    Defined in :numref:`sec_utils`
-    """
-    data_dir = d2l.download_extract('fra-eng')
-    with open(os.path.join(data_dir, 'fra.txt'), 'r') as f:
-        return f.read()
-
-
-def load_array(data_arrays, batch_size, is_train=True):
-    """Construct a PyTorch data iterator.
-
-    Defined in :numref:`sec_utils`
-    """
-    dataset = data.TensorDataset(*data_arrays)
-    return data.DataLoader(dataset, batch_size, shuffle=is_train)
-
-
-def truncate_pad(line, num_steps, padding_token):
-    """Truncate or pad sequences.
-
-    Defined in :numref:`sec_utils`
-    """
-    if len(line) > num_steps:
-        return line[:num_steps]  # Truncate
-    return line + [padding_token] * (num_steps - len(line))  # Pad
-
-
-def tokenize_nmt(text, num_examples=None):
-    """Tokenize the English-French dataset.
-
-    Defined in :numref:`sec_utils`
-    """
-    source, target = [], []
-    for i, line in enumerate(text.split('\n')):
-        if num_examples and i > num_examples:
-            break
-        parts = line.split('\t')
-        if len(parts) == 2:
-            source.append(parts[0].split(' '))
-            target.append(parts[1].split(' '))
-    return source, target
-
-
-def preprocess_nmt(text):
-    """Preprocess the English-French dataset.
-
-    Defined in :numref:`sec_utils`
-    """
-    def no_space(char, prev_char):
-        return char in set(',.!?') and prev_char != ' '
-
-    # Replace non-breaking space with space, and convert uppercase letters to
-    # lowercase ones
-    text = text.replace('\u202f', ' ').replace('\xa0', ' ').lower()
-    # Insert space between words and punctuation marks
-    out = [
-        ' ' + char if i > 0 and no_space(char, text[i - 1]) else char
-        for i, char in enumerate(text)
-    ]
-    return ''.join(out)
-
-
-def build_array_nmt(lines, vocab, num_steps):
-    """Transform text sequences of machine translation into minibatches.
-
-    Defined in :numref:`sec_utils`
-    """
-    lines = [vocab[l] for l in lines]
-    lines = [l + [vocab['<eos>']] for l in lines]
-    array = d2l.tensor(
-        [truncate_pad(l, num_steps, vocab['<pad>']) for l in lines])
-    valid_len = d2l.reduce_sum(d2l.astype(array != vocab['<pad>'], d2l.int32),
-                               1)
-    return array, valid_len
-
-
-def load_data_nmt(batch_size, num_steps, num_examples=600):
-    """Return the iterator and the vocabularies of the translation dataset.
-
-    Defined in :numref:`sec_utils`
-    """
-    text = preprocess_nmt(read_data_nmt())
-    source, target = tokenize_nmt(text, num_examples)
-    src_vocab = d2l.Vocab(source,
-                          min_freq=2,
-                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
-    tgt_vocab = d2l.Vocab(target,
-                          min_freq=2,
-                          reserved_tokens=['<pad>', '<bos>', '<eos>'])
-    src_array, src_valid_len = build_array_nmt(source, src_vocab, num_steps)
-    tgt_array, tgt_valid_len = build_array_nmt(target, tgt_vocab, num_steps)
-    data_arrays = (src_array, src_valid_len, tgt_array, tgt_valid_len)
-    data_iter = d2l.load_array(data_arrays, batch_size)
-    return data_iter, src_vocab, tgt_vocab
-
-
-if __name__ == '__mian__':
-
-    nmtdataset = NMTDatasets(download=True)
+if __name__ == '__main__':
+    root = '/Users/jianzhengnie/work_dir/code_gallery/nlp-toolkit/examples/data'
+    nmtdataset = NMTDatasets(root=root)
     print(nmtdataset)
-    data1 = nmtdataset.arrays
-    print(data1)
+    # arrays, src_vocab, tgt_vocab = nmtdataset._build_arrays(
+    #     nmtdataset._read_data())
+    data1 = nmtdataset._read_data()
+    data2 = nmtdataset._preprocess(data1)
+    src, tgt = nmtdataset._tokenize(data2)
+    # print(src, tgt)
+    train_array = nmtdataset.get_dataloader(train=True)
+    test_array = nmtdataset.get_dataloader(train=False)
+    print(train_array)
+    print(test_array)
