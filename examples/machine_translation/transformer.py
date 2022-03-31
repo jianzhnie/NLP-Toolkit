3af00960b90f770ff427c1e66ea20297226308c9
jianzhnie <jianzhnie@126.com>
2022-03-31 20:16:44 +0800

    Update transformer.py

--- examples/machine_translation/transformer.py
+++ examples/machine_translation/transformer.py
@@ -101,11 +101,11 @@ def generate_square_subsequent_mask(sz):
     return mask
 
 
-def create_mask(src, tgt, device):
+def create_mask(src, tgt):
     src_seq_len = src.shape[0]
     tgt_seq_len = tgt.shape[0]
 
-    tgt_mask = generate_square_subsequent_mask(tgt_seq_len, device=device)
+    tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
     src_mask = torch.zeros((src_seq_len, src_seq_len), ).type(torch.bool)
 
     src_padding_mask = (src == PAD_IDX).transpose(0, 1)
@@ -134,7 +134,7 @@ def train_epoch(model, train_loader, optimizer, device):
         tgt_input = tgt[:-1, :]
 
         src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = create_mask(
-            src, tgt_input, device=device)
+            src, tgt_input)
 
         logits = model(src, tgt_input, src_mask, tgt_mask, src_padding_mask,
                        tgt_padding_mask, src_padding_mask)
