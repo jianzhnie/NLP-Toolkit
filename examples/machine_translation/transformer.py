dbfff9848c92fd819ffd4c4e2928016d716646d9
jianzhnie <jianzhnie@126.com>
2022-03-31 20:13:38 +0800

    Update transformer.py

--- examples/machine_translation/transformer.py
+++ examples/machine_translation/transformer.py
@@ -94,9 +94,8 @@ class Seq2SeqTransformer(nn.Module):
             self.positional_encoding(self.tgt_tok_emb(tgt)), memory, tgt_mask)
 
 
-def generate_square_subsequent_mask(sz, device):
-    mask = (torch.triu(torch.ones((sz, sz),
-                                  device=device)) == 1).transpose(0, 1)
+def generate_square_subsequent_mask(sz):
+    mask = (torch.triu(torch.ones((sz, sz), )) == 1).transpose(0, 1)
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(
         mask == 1, float(0.0))
     return mask
@@ -107,8 +106,7 @@ def create_mask(src, tgt, device):
     tgt_seq_len = tgt.shape[0]
 
     tgt_mask = generate_square_subsequent_mask(tgt_seq_len, device=device)
-    src_mask = torch.zeros((src_seq_len, src_seq_len),
-                           devic=device).type(torch.bool)
+    src_mask = torch.zeros((src_seq_len, src_seq_len), ).type(torch.bool)
 
     src_padding_mask = (src == PAD_IDX).transpose(0, 1)
     tgt_padding_mask = (tgt == PAD_IDX).transpose(0, 1)
