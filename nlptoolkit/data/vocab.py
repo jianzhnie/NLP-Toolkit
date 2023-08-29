f73a070ab9abde70ba43cf93ff001820b39cae3a
jianzhnie <jianzhnie@126.com>
2023-08-29 11:13:01 +0800

    update nlptoolkit

--- nlptoolkit/data/vocab.py
+++ nlptoolkit/data/vocab.py
@@ -435,13 +435,13 @@ class Vocab(object):
         """
 
         token_lst = []
-        token_to_idx = collections.OrderedDict()
+        vocab = collections.OrderedDict()
         with open(vocab_file, 'r', encoding='utf-8') as reader:
             tokens = reader.readlines()
             for index, token in enumerate(tokens):
                 token = token.rstrip('\n').split('\t')[0]
-                token_to_idx[token] = index
-        token_lst = list(token_to_idx.keys())
+                vocab[token] = index
+        token_lst = list(vocab.keys())
         token_to_idx = {token: idx for idx, token in enumerate(token_lst)}
         vocab = Vocab.from_dict(token_to_idx,
                                 unk_token=unk_token,
