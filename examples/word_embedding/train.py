f8e5a523a1780d9b4f998c8fdb8a472dd5e72110
jianzhnie <jianzhnie@126.com>
2023-09-01 09:42:53 +0800

    update examples

--- examples/word_embedding/train.py
+++ examples/word_embedding/train.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Dict, List, Optional
 
 import torch
 import torch.nn as nn
@@ -8,7 +9,7 @@ from torch.utils.data import DataLoader, Dataset
 
 sys.path.append('../../')
 from nlptoolkit.data.tokenizer import JiebaTokenizer
-from nlptoolkit.data.vocab import Vocab, truncate_pad
+from nlptoolkit.data.vocab import Vocab
 
 
 class BoWClassifier(nn.Module):
@@ -86,56 +87,118 @@ class BoWClassifier(nn.Module):
 
 
 class TorchDataset(Dataset):
-    """Torch Dataset for text classification.
     """
-    def __init__(self, dataset, tokenizer: JiebaTokenizer, vocab: Vocab,
-                 max_seq_len: int):
+    PyTorch dataset for text classification or regression tasks.
+
+    Attributes:
+        dataset: List of examples, each being a dict with keys 'text' and 'label'.
+        tokenizer: Tokenizer to split text into tokens.
+        vocab: Vocabulary for mapping tokens to indices.
+        max_seq_len: Maximum sequence length after truncation.
+        label_dtype: Data type of label tensor (int64 for classification, float32 for regression).
+    """
+    def __init__(self,
+                 dataset: List[Dict],
+                 tokenizer: JiebaTokenizer,
+                 vocab: Vocab,
+                 max_seq_len: int,
+                 label_dtype: Optional[torch.dtype] = torch.int64):
+        """
+        Initialize dataset with data, tokenizer, vocab and max sequence length.
+        """
         self.dataset = dataset
         self.tokenizer = tokenizer
         self.vocab = vocab
         self.max_seq_len = max_seq_len
+        self.label_dtype = label_dtype
 
-    def __getitem__(self, index):
+    def __len__(self) -> int:
+        """Return length of dataset."""
+        return len(self.dataset)
+
+    def __getitem__(self, index: int) -> Dict:
+        """
+        Return single example by index.
+
+        Args:
+            index: Index of example.
+
+        Returns:
+            Dictionary with 'input_ids', 'valid_length', 'label' tensors.
+        """
         example = self.dataset[index]
         text = example['text']
-        label = int(example['label'])
+        label = example['label']
+
+        # Tokenize
         tokens = self.tokenizer.cut(text)
 
+        # Numericalize
         input_ids = self.vocab.to_index(tokens)
-        valid_length = torch.tensor(len(input_ids))
+        valid_length = len(input_ids)
 
-        input_ids = truncate_pad(
-            inputs=input_ids,
-            max_seq_len=self.max_seq_len,
-            padding_token_id=self.vocab.get_pad_token_id(),
-        )
+        # Truncate and pad
+        input_ids = self._truncate_pad(input_ids, self.max_seq_len,
+                                       self.vocab.get_pad_token_id())
+
+        # Convert to tensors
         input_ids = torch.tensor(input_ids, dtype=torch.int32)
-        label = torch.tensor(label, dtype=torch.int64)
+        valid_length = torch.tensor(valid_length)
+        label = torch.tensor(label, dtype=self.label_dtype)
 
         return input_ids, valid_length, label
 
-    def __len__(self):
-        return len(self.dataset)
+    def _truncate_pad(self,
+                      inputs: List[int],
+                      max_seq_len: int,
+                      padding_token_id: int = 0) -> List[int]:
+        """
+        Truncate and pad sequence to max sequence length.
+        """
+        if len(inputs) > max_seq_len:
+            inputs = inputs[:max_seq_len]
+        else:
+            inputs = inputs + [padding_token_id] * (max_seq_len - len(inputs))
+        return inputs
 
 
 def train(model: nn.Module,
           train_loader: DataLoader,
           optimizer: Adam,
-          loss_fn: None,
-          num_epoch: int = 10,
-          device: str = 'cpu'):
+          loss_fn: nn.Module,
+          num_epochs: int = 10,
+          device: torch.device = torch.device('cpu')):
 
-    for epoch in range(num_epoch):
+    model.to(device)
+    model.train()
+
+    for epoch in range(num_epochs):
+
         total_loss = 0
+
         for batch in train_loader:
-            input_ids, valid_length, labels = [x.to(device) for x in batch]
+
+            # Move batch tensors to device
+            inputs, length, labels = batch
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
             optimizer.zero_grad()
-            logits = model(input_ids)
-            loss = loss_fn(logits, labels)
+
+            # Forward pass
+            outputs = model(inputs)
+            loss = loss_fn(outputs, labels)
+
+            # Backward pass
             loss.backward()
+
+            # Update weights
             optimizer.step()
+
+            # Track loss
             total_loss += loss.item()
-        print(f'Loss: {total_loss:.2f}')
+
+        print(f'Epoch {epoch+1}, Loss: {total_loss:.4f}')
 
 
 def main():
@@ -163,14 +226,14 @@ def main():
     loss_fn = nn.CrossEntropyLoss()
     optimizer = Adam(
         model.parameters(),
-        lr=2e-4,
+        lr=1e-3,
     )
     model.to(device)
     train(model,
           train_dataloader,
           optimizer,
           loss_fn,
-          num_epoch=20,
+          num_epochs=20,
           device=device)
 
 
