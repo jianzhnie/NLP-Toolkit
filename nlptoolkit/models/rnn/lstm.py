fa897ca28aa916d6027893943c592e6f6f5d3d55
jianzhnie <jianzhnie@126.com>
2023-09-07 15:39:03 +0800

    update rnn model

--- nlptoolkit/models/rnn/lstm.py
+++ nlptoolkit/models/rnn/lstm.py
@@ -236,51 +236,6 @@ class LSTMBase(nn.Module):
         return outputs, (h_x, c_x)
 
 
-class CustomLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size):
-        super().__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.W = nn.Parameter(torch.Tensor(input_size, hidden_size * 4))
-        self.U = nn.Parameter(torch.Tensor(hidden_size, hidden_size * 4))
-        self.bias = nn.Parameter(torch.Tensor(hidden_size * 4))
-        self.init_weights()
-
-    def init_weights(self):
-        stdv = 1.0 / math.sqrt(self.hidden_size)
-        for weight in self.parameters():
-            weight.data.uniform_(-stdv, stdv)
-
-    def forward(self, x, init_states=None):
-        """Assumes x is of shape (batch, sequence, feature)"""
-        bs, seq_sz, _ = x.size()
-        hidden_seq = []
-        if init_states is None:
-            h_t, c_t = (torch.zeros(bs, self.hidden_size).to(x.device),
-                        torch.zeros(bs, self.hidden_size).to(x.device))
-        else:
-            h_t, c_t = init_states
-
-        HS = self.hidden_size
-        for t in range(seq_sz):
-            x_t = x[:, t, :]
-            # batch the computations into a single matrix multiplication
-            gates = x_t @ self.W + h_t @ self.U + self.bias
-            i_t, f_t, g_t, o_t = (
-                torch.sigmoid(gates[:, :HS]),  # input
-                torch.sigmoid(gates[:, HS:HS * 2]),  # forget
-                torch.tanh(gates[:, HS * 2:HS * 3]),
-                torch.sigmoid(gates[:, HS * 3:]),  # output
-            )
-            c_t = f_t * c_t + i_t * g_t
-            h_t = o_t * torch.tanh(c_t)
-            hidden_seq.append(h_t.unsqueeze(0))
-        hidden_seq = torch.cat(hidden_seq, dim=0)
-        # reshape from shape (sequence, batch, feature) to (batch, sequence, feature)
-        hidden_seq = hidden_seq.transpose(0, 1).contiguous()
-        return hidden_seq, (h_t, c_t)
-
-
 class BiLSTM_CRF(nn.Module):
     def __init__(self, vocab_size, tag_to_ix, embedding_dim, hidden_dim):
         super(BiLSTM_CRF, self).__init__()
