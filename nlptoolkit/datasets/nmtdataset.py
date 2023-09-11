515366b05d3aad21e70b64350b921c81c0fda270
jianzhnie <jianzhnie@126.com>
2023-09-11 11:28:44 +0800

    update nmt datasets

--- nlptoolkit/datasets/nmtdataset.py
+++ nlptoolkit/datasets/nmtdataset.py
@@ -5,43 +5,39 @@ import torch
 sys.path.append('../../')
 from typing import List, Tuple
 
+from torch.utils.data import Dataset
+
 from nlptoolkit.data.tokenizer import Tokenizer
-from nlptoolkit.data.utils.utils import truncate_pad
-from nlptoolkit.data.vocab import (BOS_TOKEN, EOS_TOKEN, PAD_TOKEN, UNK_TOKEN,
-                                   Vocab)
+from nlptoolkit.data.vocab import Vocab
 
 
-class NMTDatasets():
+class NMTDataset(Dataset):
     """Machine translation dataset.
 
     Args:
-        file_path (str): The path to the  machine translation data file.
+        file_path (str): The path to the machine translation data file.
+        max_seq_len (int): Maximum sequence length for source and target sentences.
 
+    Attributes:
+        src_vocab (Vocab): Vocabulary for source language.
+        tgt_vocab (Vocab): Vocabulary for target language.
     """
     def __init__(self, file_path: str = 'data', max_seq_len: int = 10):
-        super(NMTDatasets, self).__init__()
         self.file_path = file_path
         self.max_seq_len = max_seq_len
         self.tokenizer = Tokenizer()
-        self.src_txt, self.tgt_tx = self.load_and_preprocess_data(
-            self.file_path)
+        # Load datasets
+        self.src_texts, self.tgt_texts = self.load_and_preprocess_data()
+        # Tokenizer
         self.src_sentences, self.tgt_sentences = self._tokenize(
-            self.src_txt, self.tgt_tx)
-        self.src_vocab = Vocab.build_vocab(self.src_sentences,
-                                           min_freq=2,
-                                           pad_token=PAD_TOKEN,
-                                           unk_token=UNK_TOKEN,
-                                           bos_token=BOS_TOKEN,
-                                           eos_token=EOS_TOKEN)
-        self.tgt_vocab = Vocab.build_vocab(self.tgt_sentences,
-                                           min_freq=2,
-                                           pad_token=PAD_TOKEN,
-                                           unk_token=UNK_TOKEN,
-                                           bos_token=BOS_TOKEN,
-                                           eos_token=EOS_TOKEN)
+            self.src_texts, self.tgt_texts)
+
+        # Build vocabularies
+        self.src_vocab = self.build_vocab(self.src_sentences)
+        self.tgt_vocab = self.build_vocab(self.tgt_sentences)
 
     def load_and_preprocess_data(
-            self, file_path: str) -> Tuple[List[List[str]], List[List[str]]]:
+            self) -> Tuple[List[List[str]], List[List[str]]]:
         """
         Load and preprocess the machine translation dataset.
 
@@ -52,24 +48,24 @@ class NMTDatasets():
         """
         source_texts = []
         target_texts = []
-        with open(file_path, encoding='utf-8') as f:
-            for line in f.readlines():
+
+        with open(self.file_path, encoding='utf-8') as f:
+            for line in f:
                 line = line.replace('\u202f', ' ').replace('\xa0', ' ')
-                parts = line.split('\t')
-                if len(parts):
-                    source_texts.append(parts[0])
-                    target_texts.append(parts[1])
+                parts = line.strip().split('\t')
+                source_texts.append(parts[0])
+                target_texts.append(parts[1])
+
         assert len(source_texts) == len(
-            target_texts
-        ), 'The number of source sentences and target sentences does not match.'
+            target_texts), 'Source and target sentence counts do not match.'
 
         return source_texts, target_texts
 
-    def _tokenize(self, src_texts, tgt_texts):
+    def _tokenize(self, source_texts, target_texts):
         """Tokenize the  dataset."""
         src_sentences = []
         tgt_sentences = []
-        for src_text, tgt_text in zip(src_texts, tgt_texts):
+        for src_text, tgt_text in zip(source_texts, target_texts):
             src_tokens = self.tokenizer.tokenize(src_text)
             tgt_tokens = self.tokenizer.tokenize(tgt_text)
             src_sentences.append(src_tokens)
@@ -80,30 +76,74 @@ class NMTDatasets():
 
         return src_sentences, tgt_sentences
 
-    def __getitem__(self, idx):
-        src_txt = self.src_sentences[idx]
-        tgt_txt = self.tgt_sentences[idx]
-        src_tokens = self.src_vocab.to_index(src_txt)
-        src_tokens = truncate_pad(src_tokens, self.max_seq_len,
-                                  self.src_vocab['<pad>'])
-        tgt_tokens = self.tgt_vocab.to_index(tgt_txt)
-        tgt_tokens = truncate_pad(tgt_tokens, self.max_seq_len,
-                                  self.tgt_vocab['<pad>'])
-        src_tensor, tgt_tensor = self._to_tensor(src_tokens, tgt_tokens)
+    def build_vocab(self, sentences: List[List[str]]) -> Vocab:
+        """Build a vocabulary from a list of tokenized sentences.
+
+        Args:
+            sentences (List[List[str]]): List of tokenized sentences.
+
+        Returns:
+            Vocab: Vocabulary object.
+        """
+        return Vocab.build_vocab(sentences,
+                                 min_freq=2,
+                                 pad_token='<pad>',
+                                 unk_token='<unk>',
+                                 bos_token='<bos>',
+                                 eos_token='<eos>')
+
+    def __getitem__(self, idx) -> Tuple[torch.Tensor, torch.Tensor]:
+        src_tokens = self.src_vocab.to_index(self.src_sentences[idx])
+        tgt_tokens = self.tgt_vocab.to_index(self.tgt_sentences[idx])
+
+        # Ensure the sequences have the maximum sequence length
+        src_tokens = self.truncate_pad(src_tokens, self.max_seq_len,
+                                       self.src_vocab['<pad>'])
+        tgt_tokens = self.truncate_pad(tgt_tokens, self.max_seq_len,
+                                       self.tgt_vocab['<pad>'])
+
+        src_tensor = self.to_tensor(src_tokens)
+        tgt_tensor = self.to_tensor(tgt_tokens)
         return src_tensor, tgt_tensor
 
-    def __len__(self):
-        return len(self.src_txt)
+    def __len__(self) -> int:
+        return len(self.src_sentences)
 
-    def _to_tensor(self, src_tokens, tgt_tokens):
-        src_tensor = torch.tensor(src_tokens, dtype=torch.long)
-        tgt_tensor = torch.tensor(tgt_tokens, dtype=torch.long)
-        return src_tensor, tgt_tensor
+    @staticmethod
+    def truncate_pad(tokens: List[int], max_length: int,
+                     pad_token: int) -> List[int]:
+        """Pad or truncate a list of tokens to a specified length.
+
+        Args:
+            tokens (List[int]): List of tokens.
+            max_length (int): Maximum length to pad or truncate to.
+            pad_token (int): Padding token.
+
+        Returns:
+            List[int]: Padded or truncated list of tokens.
+        """
+        if len(tokens) < max_length:
+            tokens.extend([pad_token] * (max_length - len(tokens)))
+        elif len(tokens) > max_length:
+            tokens = tokens[:max_length]
+        return tokens
+
+    @staticmethod
+    def to_tensor(tokens: List[int]) -> torch.Tensor:
+        """Convert a list of tokens to a PyTorch tensor.
+
+        Args:
+            tokens (List[int]): List of tokens.
+
+        Returns:
+            torch.Tensor: PyTorch tensor.
+        """
+        return torch.tensor(tokens, dtype=torch.long)
 
 
 if __name__ == '__main__':
     root = '/home/robin/work_dir/llm/nlp-toolkit/data/nmt/fra-eng/fra.txt'
-    nmtdataset = NMTDatasets(file_path=root)
+    nmtdataset = NMTDataset(file_path=root)
     for idx in range(10):
         src_tensor, tgt_tensor = nmtdataset[idx]
         print(src_tensor, tgt_tensor)
