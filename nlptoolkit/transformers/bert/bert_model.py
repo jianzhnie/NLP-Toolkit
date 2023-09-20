794e5fc73b1ee959c8d51f24e2714ca905e8f2ca
jianzhnie <jianzhnie@126.com>
2023-09-20 20:05:12 +0800

    update nlptoolkit

--- nlptoolkit/transformers/bert/bert_model.py
+++ nlptoolkit/transformers/bert/bert_model.py
@@ -18,23 +18,77 @@ from nlptoolkit.transformers.vanilla import (TransformerEncoder,
 
 
 class BertEmbedding(nn.Module):
-    def __init__(self, vocab_size, hidden_size, n_segments, max_len=1000):
+    def __init__(self,
+                 vocab_size,
+                 hidden_size,
+                 type_vocab_size,
+                 hidden_dropout_prob=0.1,
+                 max_position_embeddings=1000,
+                 position_embedding_type='absolute'):
         super().__init__()
-        self.token_embedding = nn.Embedding(vocab_size, hidden_size)
-        self.segment_embedding = nn.Embedding(n_segments, hidden_size)
-        self.pos_embedding = nn.Embedding(max_len, hidden_size)
+        self.word_embedding = nn.Embedding(vocab_size,
+                                           hidden_size,
+                                           padding_idx=0)
+        self.position_embedding = nn.Embedding(max_position_embeddings,
+                                               hidden_size)
+        self.token_type_embedding = nn.Embedding(type_vocab_size, hidden_size)
 
-        self.norm = nn.LayerNorm(hidden_size)
+        self.LayerNorm = nn.LayerNorm(hidden_size)
+        self.dropout = nn.Dropout(hidden_dropout_prob)
+        self.position_embedding_type = position_embedding_type
+        self.register_buffer('position_ids',
+                             torch.arange(max_position_embeddings).expand(
+                                 (1, -1)),
+                             persistent=False)
+        self.register_buffer('token_type_ids',
+                             torch.zeros(self.position_ids.size(),
+                                         dtype=torch.long),
+                             persistent=False)
 
-    def forward(self, inputs, segments):
-        bat_size, seq_len, _ = inputs.shape
-        pos = torch.arange(seq_len, dtype=torch.long)
-        pos = pos.unsqueeze(0).expand_as(inputs)
-        # (seq_len, ) --> (batch_size, seq_len)
-        embedding = self.token_embedding(inputs) + self.pos_embedding(
-            pos) + self.segment_embedding(segments)
-        embedding = self.norm(embedding)
-        return embedding
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        inputs_embeds: Optional[torch.LongTensor] = None,
+        past_key_values_length: int = 0,
+    ):
+        if input_ids is not None:
+            input_shape = input_ids.size()
+        else:
+            input_shape = inputs_embeds.size()[:-1]
+
+        seq_length = input_shape[1]
+
+        if position_ids is None:
+            position_ids = self.position_ids[:, past_key_values_length:
+                                             seq_length +
+                                             past_key_values_length]
+        # Setting the token_type_ids to the registered buffer in constructor where it is all zeros, which usually occurs
+        # when its auto-generated, registered buffer helps users when tracing the model without passing token_type_ids, solves
+        # issue #5664
+        if token_type_ids is None:
+            if hasattr(self, 'token_type_ids'):
+                buffered_token_type_ids = self.token_type_ids[:, :seq_length]
+                buffered_token_type_ids_expanded = buffered_token_type_ids.expand(
+                    input_shape[0], seq_length)
+                token_type_ids = buffered_token_type_ids_expanded
+            else:
+                token_type_ids = torch.zeros(input_shape,
+                                             dtype=torch.long,
+                                             device=self.position_ids.device)
+
+        if inputs_embeds is None:
+            inputs_embeds = self.word_embedding(input_ids)
+        token_type_embeddings = self.token_type_embedding(token_type_ids)
+
+        embeddings = inputs_embeds + token_type_embeddings
+        if self.position_embedding_type == 'absolute':
+            position_embeddings = self.position_embedding(position_ids)
+            embeddings += position_embeddings
+        embeddings = self.LayerNorm(embeddings)
+        embeddings = self.dropout(embeddings)
+        return embeddings
 
 
 class MaskLM(nn.Module):
