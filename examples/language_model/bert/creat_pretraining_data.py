7761862a0491d41b4e89b773f3a5974c7334926a
jianzhnie <jianzhnie@126.com>
2023-10-10 16:08:29 +0800

    update create bert data

---
+++ examples/language_model/bert/creat_pretraining_data.py
@@ -0,0 +1,492 @@
+import collections
+import random
+from typing import List, NamedTuple, Tuple
+
+import h5py
+import numpy as np
+from tqdm import tqdm
+from transformers import PreTrainedTokenizer
+
+from nlptoolkit.llms.bert.bert_tokenizer import (BertTokenizer,
+                                                 convert_to_unicode)
+
+
+# Define a named tuple called MaskedLmInstance
+class MaskedLmInstance(NamedTuple):
+    """
+    Represents a masked language model instance with index and label.
+
+    Attributes:
+        index (int): Index of the masked token.
+        label (str): Label for the masked token, typically "[MASK]".
+    """
+    index: int
+    label: str
+
+
+class TrainingInstance:
+    """
+    A single training instance (sentence pair) for masked language model training.
+
+    Attributes:
+        tokens (List[str]): List of tokens representing the input sentence pair.
+        segment_ids (List[int]): List of segment IDs indicating token belongingness to sentences.
+        masked_lm_positions (List[int]): List of positions where tokens are masked during training.
+        masked_lm_labels (List[str]): List of masked tokens corresponding to masked positions.
+        is_random_next (bool): Indicates if the next sentence is randomly chosen.
+    """
+    def __init__(
+        self,
+        tokens: List[str],
+        segment_ids: List[int],
+        masked_lm_positions: List[int],
+        masked_lm_labels: List[str],
+        is_random_next: bool,
+    ):
+        """
+        Initializes a TrainingInstance object.
+
+        Args:
+            tokens (List[str]): List of tokens representing the input sentence pair.
+            segment_ids (List[int]): List of segment IDs indicating token belongingness to sentences.
+            masked_lm_positions (List[int]): List of positions where tokens are masked during training.
+            masked_lm_labels (List[str]): List of masked tokens corresponding to masked positions.
+            is_random_next (bool): Indicates if the next sentence is randomly chosen.
+        """
+        self.tokens = tokens
+        self.segment_ids = segment_ids
+        self.masked_lm_positions = masked_lm_positions
+        self.masked_lm_labels = masked_lm_labels
+        self.is_random_next = is_random_next
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the TrainingInstance object.
+
+        Returns:
+            str: String representation of the TrainingInstance.
+        """
+        tokens_str = ' '.join(self.tokens)
+        segment_ids_str = ' '.join(map(str, self.segment_ids))
+        masked_positions_str = ' '.join(map(str, self.masked_lm_positions))
+        masked_labels_str = ' '.join(self.masked_lm_labels)
+
+        outputs = ''
+        outputs += f'Tokens: {tokens_str}\n'
+        outputs += f'Segment IDs: {segment_ids_str}\n'
+        outputs += f'Masked Positions: {masked_positions_str}\n'
+        outputs += f'Masked Labels: {masked_labels_str}\n'
+        outputs += f'Is Random Next: {self.is_random_next}'
+
+        return outputs
+
+    def __repr__(self) -> str:
+        """
+        Returns a string representation of the TrainingInstance object.
+
+        Returns:
+            str: String representation of the TrainingInstance.
+        """
+        return self.__str__()
+
+
+def write_instance_to_example_file(
+    instances: List[TrainingInstance],
+    tokenizer: PreTrainedTokenizer,
+    max_seq_length: int,
+    max_predictions_per_seq: int,
+    output_file: str,
+) -> None:
+    """
+    Create TF example files from `TrainingInstance`s and save them in HDF5 format.
+
+    Args:
+        instances (List[TrainingInstance]): List of TrainingInstance objects.
+        tokenizer (PreTrainedTokenizer): Tokenizer object from the transformers library.
+        max_seq_length (int): Maximum sequence length for the input.
+        max_predictions_per_seq (int): Maximum number of masked LM predictions per sequence.
+        output_file (str): Path to save the HDF5 output file.
+
+    Returns:
+        None
+    """
+    total_written = 0
+    features = collections.OrderedDict()
+
+    num_instances = len(instances)
+    features['input_ids'] = np.zeros([num_instances, max_seq_length],
+                                     dtype='int32')
+    features['input_mask'] = np.zeros([num_instances, max_seq_length],
+                                      dtype='int32')
+    features['segment_ids'] = np.zeros([num_instances, max_seq_length],
+                                       dtype='int32')
+    features['masked_lm_positions'] = np.zeros(
+        [num_instances, max_predictions_per_seq], dtype='int32')
+    features['masked_lm_ids'] = np.zeros(
+        [num_instances, max_predictions_per_seq], dtype='int32')
+    features['next_sentence_labels'] = np.zeros(num_instances, dtype='int32')
+
+    for inst_index, instance in enumerate(
+            tqdm(instances, desc='Processing instances')):
+        input_ids = tokenizer.convert_tokens_to_ids(instance.tokens)
+        input_mask = [1] * len(input_ids)
+        segment_ids = list(instance.segment_ids)
+        assert len(input_ids) <= max_seq_length
+
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+
+        masked_lm_positions = list(instance.masked_lm_positions)
+        masked_lm_ids = tokenizer.convert_tokens_to_ids(
+            instance.masked_lm_labels)
+        masked_lm_weights = [1.0] * len(masked_lm_ids)
+
+        while len(masked_lm_positions) < max_predictions_per_seq:
+            masked_lm_positions.append(0)
+            masked_lm_ids.append(0)
+            masked_lm_weights.append(0.0)
+
+        next_sentence_label = 1 if instance.is_random_next else 0
+
+        features['input_ids'][inst_index] = input_ids
+        features['input_mask'][inst_index] = input_mask
+        features['segment_ids'][inst_index] = segment_ids
+        features['masked_lm_positions'][inst_index] = masked_lm_positions
+        features['masked_lm_ids'][inst_index] = masked_lm_ids
+        features['next_sentence_labels'][inst_index] = next_sentence_label
+
+        total_written += 1
+
+    print(f'Total instances processed: {total_written}')
+    print('Saving data to HDF5 file...')
+    with h5py.File(output_file, 'w') as f:
+        for feature_name, feature_data in features.items():
+            f.create_dataset(feature_name,
+                             data=feature_data,
+                             dtype=feature_data.dtype,
+                             compression='gzip')
+    f.flush()
+    f.close()
+    print(f'Data saved to {output_file}')
+
+
+def create_training_instances(
+    input_files: List[str],
+    tokenizer: BertTokenizer,
+    max_seq_length: int,
+    dupe_factor: int,
+    short_seq_prob: float,
+    masked_lm_prob: float,
+    max_predictions_per_seq: int,
+    random_generator: random.Random,
+) -> List[TrainingInstance]:
+    """
+    Create `TrainingInstance`s from raw text.
+
+    Args:
+        input_files (List[str]): List of input file paths containing raw text data.
+        tokenizer (Any): Tokenizer object for tokenizing the input text.
+        max_seq_length (int): Maximum sequence length.
+        dupe_factor (int): Number of times to duplicate the input data for varied instances.
+        short_seq_prob (float): Probability of using shorter sequences.
+        masked_lm_prob (float): Probability of masking tokens.
+        max_predictions_per_seq (int): Maximum number of masked tokens in a sequence.
+        random_generator (random.Random): Random number generator for reproducibility.
+
+    Returns:
+        List['TrainingInstance']: List of TrainingInstance objects.
+    """
+    # Initialize a list to store tokenized documents
+    all_documents = [[]]
+
+    # Input file format:
+    # (1) One sentence per line. These should ideally be actual sentences, not
+    # entire paragraphs or arbitrary spans of text. (Because we use the
+    # sentence boundaries for the "next sentence prediction" task).
+    # (2) Blank lines between documents. Document boundaries are needed so
+    # that the "next sentence prediction" task doesn't span between documents.
+
+    # Read raw text from input files and tokenize them
+    for input_file in input_files:
+        print('Creating instances from {}'.format(input_file))
+        with open(input_file, 'r', encoding='utf-8') as reader:
+            while True:
+                line = convert_to_unicode(reader.readline())
+                if not line:
+                    break
+                line = line.strip()
+
+                # Empty lines are used as document delimiters
+                if not line:
+                    all_documents.append([])
+                tokens = tokenizer.tokenize(line)
+                if tokens:
+                    all_documents[-1].append(tokens)
+
+    # Remove empty documents
+    all_documents = [x for x in all_documents if x]
+    random_generator.shuffle(all_documents)
+
+    # Get vocabulary words from the tokenizer
+    vocab_words = list(tokenizer.vocab.keys())
+
+    # Generate instances by duplicating and shuffling the documents
+    instances = []
+    for _ in range(dupe_factor):
+        for document_index in range(len(all_documents)):
+            instances.extend(
+                create_instances_from_document(all_documents, document_index,
+                                               max_seq_length, short_seq_prob,
+                                               masked_lm_prob,
+                                               max_predictions_per_seq,
+                                               vocab_words, random_generator))
+
+    # Shuffle the instances for randomness
+    random_generator.shuffle(instances)
+    return instances
+
+
+def create_instances_from_document(
+    all_documents: List[List[List[str]]],
+    document_index: int,
+    max_seq_length: int,
+    short_seq_prob: float,
+    masked_lm_prob: float,
+    max_predictions_per_seq: int,
+    vocab_words: List[str],
+    random_generator: random.Random,
+) -> List[TrainingInstance]:
+    """
+    Creates `TrainingInstance`s for a single document.
+
+    Args:
+        all_documents (List[List[List[str]]]): List of documents where each document is a list of segments (lists of tokens).
+        document_index (int): Index of the document to process.
+        max_seq_length (int): Maximum sequence length.
+        short_seq_prob (float): Probability of using shorter sequences.
+        masked_lm_prob (float): Probability of masking tokens.
+        max_predictions_per_seq (int): Maximum number of masked tokens in a sequence.
+        vocab_words (List[str]): Vocabulary words.
+        random_generator (Any): Random number generator for reproducibility.
+
+    Returns:
+        List[TrainingInstance]: List of TrainingInstance objects.
+    """
+    document = all_documents[document_index]
+
+    # Account for [CLS], [SEP], [SEP]
+    max_num_tokens = max_seq_length - 3
+
+    # We *usually* want to fill up the entire sequence since we are padding
+    # to `max_seq_length` anyways, so short sequences are generally wasted
+    # computation. However, we *sometimes*
+    # (i.e., short_seq_prob == 0.1 == 10% of the time) want to use shorter
+    # sequences to minimize the mismatch between pre-training and fine-tuning.
+    # The `target_seq_length` is just a rough target however, whereas
+    # `max_seq_length` is a hard limit.
+
+    # Determine target sequence length with some randomness
+    target_seq_length = max_num_tokens
+    if random_generator.random() < short_seq_prob:
+        target_seq_length = random_generator.randint(2, max_num_tokens)
+
+    # We DON'T just concatenate all of the tokens from a document into a long
+    # sequence and choose an arbitrary split point because this would make the
+    # next sentence prediction task too easy. Instead, we split the input into
+    # segments "A" and "B" based on the actual "sentences" provided by the user
+    # input.
+
+    instances = []
+    current_chunk = []
+    current_length = 0
+    i = 0
+
+    while i < len(document):
+        segment = document[i]
+        current_chunk.append(segment)
+        current_length += len(segment)
+
+        if i == len(document) - 1 or current_length >= target_seq_length:
+            if current_chunk:
+                # Determine how many segments from `current_chunk` go into the `A` (first) sentence.
+                a_end = 1
+                if len(current_chunk) >= 2:
+                    a_end = random_generator.randint(1, len(current_chunk) - 1)
+
+                tokens_a = [
+                    token for segment in current_chunk[:a_end]
+                    for token in segment
+                ]
+                tokens_b = []
+                # Random next
+                is_random_next = False
+                if len(current_chunk) == 1 or random_generator.random() < 0.5:
+                    is_random_next = True
+                    target_b_length = target_seq_length - len(tokens_a)
+
+                    # This should rarely go for more than one iteration for large
+                    # corpora. However, just to be careful, we try to make sure that
+                    # the random document is not the same as the document
+                    # we're processing.
+                    for _ in range(10):
+                        random_document_index = random_generator.randint(
+                            0,
+                            len(all_documents) - 1)
+                        if random_document_index != document_index:
+                            break
+
+                    # If picked random document is the same as the current document
+                    if random_document_index == document_index:
+                        is_random_next = False
+
+                    random_document = all_documents[random_document_index]
+                    random_start = random_generator.randint(
+                        0,
+                        len(random_document) - 1)
+                    for j in range(random_start, len(random_document)):
+                        tokens_b.extend(random_document[j])
+                        if len(tokens_b) >= target_b_length:
+                            break
+                    # We didn't actually use these segments so we "put them back" so
+                    # they don't go to waste.
+                    num_unused_segments = len(current_chunk) - a_end
+                    i -= num_unused_segments
+                # Actual next
+                else:
+                    is_random_next = False
+                    for j in range(a_end, len(current_chunk)):
+                        tokens_b.extend(current_chunk[j])
+
+                truncate_seq_pair(tokens_a, tokens_b, max_num_tokens,
+                                  random_generator)
+
+                assert len(tokens_a) >= 1
+                assert len(tokens_b) >= 1
+
+                # Combine tokens from tokens_a and tokens_b into a single list of tokens
+                tokens = ['[CLS]'] + tokens_a + ['[SEP]'
+                                                 ] + tokens_b + ['[SEP]']
+                segment_ids = [0] * (len(tokens_a) +
+                                     2) + [1] * (len(tokens_b) + 1)
+
+                (tokens, masked_lm_positions,
+                 masked_lm_labels) = create_masked_lm_predictions(
+                     tokens, masked_lm_prob, max_predictions_per_seq,
+                     vocab_words, random_generator)
+
+                instance = TrainingInstance(
+                    tokens=tokens,
+                    segment_ids=segment_ids,
+                    is_random_next=is_random_next,
+                    masked_lm_positions=masked_lm_positions,
+                    masked_lm_labels=masked_lm_labels)
+                instances.append(instance)
+            current_chunk = []
+            current_length = 0
+        i += 1
+
+    return instances
+
+
+def create_masked_lm_predictions(
+    tokens: List[str],
+    masked_lm_prob: float,
+    max_predictions_per_seq: int,
+    vocab_words: List[str],
+    random_generator: random.Random,
+) -> Tuple[List[str], List[int], List[str]]:
+    """
+    Creates predictions for the masked LM objective.
+
+    Args:
+        tokens (List[str]): List of input tokens.
+        masked_lm_prob (float): Probability of masking tokens.
+        max_predictions_per_seq (int): Maximum number of masked tokens in the sequence.
+        vocab_words (List[str]): Vocabulary words.
+        random_generator (Any): Random number generator for reproducibility.
+
+    Returns:
+        Tuple[List[str], List[int], List[str]]: Output tokens, masked LM positions, and masked LM labels.
+    """
+    # Get candidate indices (excluding [CLS] and [SEP])
+    cand_indexes = [
+        i for i, token in enumerate(tokens) if token not in ['[CLS]', '[SEP]']
+    ]
+    random_generator.shuffle(cand_indexes)
+
+    # Calculate the number of tokens to predict
+    num_to_predict = min(max_predictions_per_seq,
+                         max(1, int(round(len(tokens) * masked_lm_prob))))
+    masked_lms = []
+    covered_indexes = set()
+
+    output_tokens = list(tokens)
+
+    # Iterate through candidate indices and mask tokens
+    for index in cand_indexes:
+        if len(masked_lms) >= num_to_predict:
+            break
+        if index in covered_indexes:
+            continue
+        covered_indexes.add(index)
+
+        masked_token = None
+        # 80% of the time, replace with [MASK]
+        if random_generator.random() < 0.8:
+            masked_token = '[MASK]'
+        else:
+            # 10% of the time, keep original
+            if random_generator.random() < 0.5:
+                masked_token = tokens[index]
+            # 10% of the time, replace with random word from vocabulary
+            else:
+                masked_token = random.choice(vocab_words)
+
+        # Update the output tokens and create MaskedLmInstance
+        output_tokens[index] = masked_token
+        masked_lms.append(MaskedLmInstance(index=index, label=masked_token))
+
+    # Sort masked_lms by index for consistency
+    masked_lms = sorted(masked_lms, key=lambda x: x.index)
+
+    # Extract masked LM positions and labels
+    masked_lm_positions = [p.index for p in masked_lms]
+    masked_lm_labels = [p.label for p in masked_lms]
+
+    return output_tokens, masked_lm_positions, masked_lm_labels
+
+
+def truncate_seq_pair(
+    tokens_a: List[str],
+    tokens_b: List[str],
+    max_num_tokens: int,
+    random_generator: random.Random,
+) -> None:
+    """
+    Truncates a pair of sequences to a maximum sequence length.
+
+    Args:
+        tokens_a (List[str]): List of tokens from the first sequence.
+        tokens_b (List[str]): List of tokens from the second sequence.
+        max_num_tokens (int): Maximum sequence length.
+        random_generator (random.Random): Random number generator for reproducibility.
+
+    Returns:
+        None
+    """
+    while len(tokens_a) + len(tokens_b) > max_num_tokens:
+        # Determine which sequence to truncate from (front or back) for more randomness
+        trunc_tokens = tokens_a if len(tokens_a) > len(tokens_b) else tokens_b
+        assert len(trunc_tokens) >= 1
+
+        # Randomly truncate from the front or the back
+        if random_generator.random() < 0.5:
+            del trunc_tokens[0]  # Truncate from the front
+        else:
+            trunc_tokens.pop()  # Truncate from the back
