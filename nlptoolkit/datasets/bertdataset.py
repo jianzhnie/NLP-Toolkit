e0b2e20f9e808f52af368bc148671379cb183c0f
jianzhnie <jianzhnie@126.com>
2023-10-13 20:33:07 +0800

    update BertDatasets

--- nlptoolkit/datasets/bertdataset.py
+++ nlptoolkit/datasets/bertdataset.py
@@ -8,7 +8,7 @@ Description:
 '''
 import os
 import random
-from typing import List
+from typing import List, Tuple
 
 from torch.utils.data import Dataset
 
@@ -17,15 +17,24 @@ from nlptoolkit.data.vocab import Vocab
 
 
 class BertDataSet(Dataset):
+    """
+    Dataset class for BERT pretraining tasks.
+
+    Args:
+        data_dir (str): Directory containing the data files.
+        data_split (str): Split of the data ('train', 'val', etc.).
+        tokenizer (Tokenizer): Tokenizer object for tokenizing the text.
+        max_seq_len (int): Maximum sequence length for BERT input.
+    """
     def __init__(
             self,
-            data_dir,
+            data_dir: str,
             data_split: str = 'train',
             tokenizer: Tokenizer = Tokenizer(),
             max_seq_len: int = 512,
     ) -> None:
-        super().__init__()
 
+        super().__init__()
         self.max_seq_len = max_seq_len
         self.tokenizer = tokenizer
         self.data_dir = os.path.join(data_dir, data_split + '.txt')
@@ -33,49 +42,108 @@ class BertDataSet(Dataset):
         self.tokenized_paragraphs = [
             tokenizer.tokenize(paragraph) for paragraph in self.paragraphs
         ]
-        self.tokenized_sentences = [
+        self.vocab: Vocab = self.build_vocab()
+        self.vocab_words = self.vocab.token_to_idx.keys()
+
+    def build_vocab(self) -> List[str]:
+        """
+        Build vocabulary from tokenized sentences.
+
+        Returns:
+            List[str]: List of vocabulary words.
+        """
+
+        tokenized_sentences = [
             sentence for paragraph in self.tokenized_paragraphs
             for sentence in paragraph
         ]
-        self.vocab: Vocab = Vocab.build_vocab(self.tokenized_sentences,
-                                              min_freq=1,
-                                              unk_token='<unk>',
-                                              pad_token='<pad>',
-                                              bos_token='<bos>',
-                                              eos_token='<eos>')
-        self.vocab_words = self.vocab.token_to_idx.keys()
+        vocab = Vocab.build_vocab(tokenized_sentences,
+                                  min_freq=1,
+                                  unk_token='<unk>',
+                                  pad_token='<pad>',
+                                  bos_token='<bos>',
+                                  eos_token='<eos>')
+        return vocab
 
     def get_bert_data(self, paragraphs, max_seq_len):
-        # 获取下一句子预测任务的数据
         examples = []
         for paragraph in paragraphs:
-            examples.extend(
-                self.get_nsp_data_from_paragraph(paragraph, paragraphs,
-                                                 max_seq_len))
+            # 获取下一句子预测任务的数据
+            nsp_data_from_paragraph = self.get_nsp_data_from_paragraph(
+                paragraph, paragraphs, max_seq_len)
+            examples.extend(nsp_data_from_paragraph)
         # 获取遮蔽语言模型任务的数据
-        examples = [(self.get(tokens, self.vocab) + (segments, is_next))
-                    for tokens, segments, is_next in examples]
+        examples = [
+            (self.get_mlm_data_from_tokens(tokens) + (segments, is_next))
+            for tokens, segments, is_next in examples
+        ]
+        return examples
 
-    def get_next_sentence(self, sentence, next_sentence, paragraphs):
+    def get_next_sentence(
+            self, sentence: List[str], next_sentence: List[str],
+            paragraphs: List[List[List[str]]]) -> Tuple[List[str], bool]:
+        """
+        Randomly selects the next sentence for NSP task.
+
+        Args:
+            sentence (List[str]): Current sentence.
+            next_sentence: (List[str]): Next sentence.
+            paragraphs (List[List[str]]): List of paragraphs.
+
+        Returns:
+            Tuple[List[str], bool]: Tokens of next sentence and whether they are consecutive sentences.
+        """
         if random.random() < 0.5:
             is_next = True
         else:
-            # paragraphs是三重列表的嵌套
+            # paragraphs 是三重列表的嵌套
             next_sentence = random.choice(random.choice(paragraphs))
             is_next = False
         return sentence, next_sentence, is_next
 
-    def get_nsp_data_from_paragraph(self, paragraph, paragraphs, max_len):
+    def get_nsp_data_from_paragraph(
+            self, paragraph: List[str], paragraphs: List[List[List[str]]],
+            max_seq_len) -> List[Tuple[List[str], bool]]:
+        """
+        Generate NSP (Next Sentence Prediction) data from a paragraph.
+
+        Args:
+            paragraph (List[str]): Tokenized paragraph.
+
+        Returns:
+            List[Tuple[List[str], bool]]: List of NSP data tuples.
+        """
         nsp_data_from_paragraph = []
         for i in range(len(paragraph) - 1):
             tokens_a, tokens_b, is_next = self.get_next_sentence(
                 paragraph[i], paragraph[i + 1], paragraphs)
             # 考虑1个'<cls>'词元和2个'<sep>'词元
-            if len(tokens_a) + len(tokens_b) + 3 > max_len:
+            if len(tokens_a) + len(tokens_b) + 3 > max_seq_len:
                 continue
             tokens, segments = self.get_tokens_and_segments(tokens_a, tokens_b)
             nsp_data_from_paragraph.append((tokens, segments, is_next))
         return nsp_data_from_paragraph
+
+    def get_tokens_and_segments(
+            self,
+            tokens_a: List[str],
+            tokens_b: List[str] = None) -> Tuple[List[str], List[int]]:
+        """
+        Get tokens and segments for BERT input.
+
+        Args:
+            tokens_a (List[str]): Tokens of the sentence.
+
+        Returns:
+            Tuple[List[str], List[int]]: Tokens and corresponding segments.
+        """
+        tokens = ['<cls>'] + tokens_a + ['<sep>']
+        # 0 and 1 are marking segment A and B, respectively
+        segments = [0] * (len(tokens_a) + 2)
+        if tokens_b is not None:
+            tokens += tokens_b + ['<sep>']
+            segments += [1] * (len(tokens_b) + 1)
+        return tokens, segments
 
     def get_mlm_data_from_tokens(self, tokens):
         candidate_pred_positions = []
@@ -95,18 +163,6 @@ class BertDataSet(Dataset):
         mlm_pred_labels = [v[1] for v in pred_positions_and_labels]
         return self.vocab[mlm_input_tokens], pred_positions, self.vocab[
             mlm_pred_labels]
-
-    def get_tokens_and_segments(self, tokens_a, tokens_b=None):
-        """Get tokens of the BERT input sequence and their segment IDs.
-
-        Defined in :numref:`sec_bert`"""
-        tokens = ['<cls>'] + tokens_a + ['<sep>']
-        # 0 and 1 are marking segment A and B, respectively
-        segments = [0] * (len(tokens_a) + 2)
-        if tokens_b is not None:
-            tokens += tokens_b + ['<sep>']
-            segments += [1] * (len(tokens_b) + 1)
-        return tokens, segments
 
     def replace_mskelm_tokens(self, tokens, candidate_pred_positions,
                               num_mlm_preds, vocab_words):
@@ -136,19 +192,45 @@ class BertDataSet(Dataset):
 
     def preprocess_text_data(self, path: str) -> List[List[str]]:
         """
+        Preprocesses the text data from the specified file.
+
         Args:
-            path (str): The path to the text file.
+            path (str): Path to the text file.
 
         Returns:
-            List[List[str]]: A list of tokenized sentences.
+            List[List[str]]: List of tokenized sentences.
         """
         assert os.path.exists(path)
         paragraphs = []
         with open(path, 'r', encoding='utf8') as f:
             lines = f.readlines()
             for line in lines:
-                if line.split(' . ') >= 2:
-                    paragraph = line.strip().lower().split(' . ')
+                if len(line.split('. ')) >= 2:
+                    paragraph = line.strip().lower().split('. ')
                     paragraphs.append(paragraph)
-
         return paragraphs
+
+    def __len__(self) -> int:
+        """
+        Returns the total number of paragraphs in the dataset.
+
+        Returns:
+            int: Number of paragraphs.
+        """
+        return len(self.paragraphs)
+
+    def __getitem__(self, idx: int) -> List[Tuple[List[str], List[int], bool]]:
+        """
+        Retrieves the NSP data for a specific paragraph.
+
+        Args:
+            idx (int): Index of the paragraph.
+
+        Returns:
+            List[Tuple[List[str], List[int], bool]]: List of NSP data tuples.
+        """
+        paragraph = self.paragraphs[idx]
+        examples = self.get_nsp_data_from_paragraph(paragraph)
+        examples = [(self.get_tokens_and_segments(tokens), is_next)
+                    for tokens, _, is_next in examples]
+        return examples
