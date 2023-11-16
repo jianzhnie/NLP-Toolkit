a6b34fbd7b6c5f4ffce823f7d4bb75d39bcc472d
jianzhnie <jianzhnie@126.com>
2023-11-16 11:10:31 +0800

    update bert model

--- nlptoolkit/llms/bert/modeling_bert.py
+++ nlptoolkit/llms/bert/modeling_bert.py
@@ -13,7 +13,7 @@ from typing import Optional, Tuple, Union
 
 import torch
 from torch import Tensor, nn
-from transformers.modeling_utils import PreTrainedModel
+from transformers import BertPreTrainedModel
 
 from .config_bert import BertConfig
 from .modeling_output import BertEncoderOutput, BertModelOutput
@@ -34,7 +34,7 @@ ACT2FN = {
     'gelu': gelu,
     'tanh': nn.functional.tanh,
     'relu': nn.functional.relu,
-    'swish': swish
+    'swish': swish,
 }
 
 
@@ -45,7 +45,7 @@ class BertEmbedding(nn.Module):
     Args:
         config (BertConfig): Configuration object containing model parameters.
     """
-    def __init__(self, config: BertConfig):
+    def __init__(self, config: BertConfig) -> None:
         super().__init__()
         self.word_embedding = nn.Embedding(config.vocab_size,
                                            config.hidden_size,
@@ -741,51 +741,6 @@ class BertPreTrainingHeads(nn.Module):
         # Predict next sentence relationship using NSP head
         seq_relationship_score = self.nsp_head(pooled_output)
         return prediction_scores, seq_relationship_score
-
-
-class BertPreTrainedModel(PreTrainedModel):
-    """
-    An abstract class to handle weights initialization and a simple interface for downloading and loading pretrained
-    models.
-    """
-
-    config_class = BertConfig
-    base_model_prefix = 'bert'
-    supports_gradient_checkpointing = True
-
-    def _init_weights(self, module: nn.Module):
-        """
-        Initialize the weights of the neural network module.
-
-        Args:
-            module (nn.Module): A PyTorch module.
-        """
-        if isinstance(module, nn.Linear):
-            module.weight.data.normal_(mean=0.0,
-                                       std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0,
-                                       std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
-
-    def _set_gradient_checkpointing(self,
-                                    module: nn.Module,
-                                    value: bool = False):
-        """
-        Set the gradient checkpointing attribute of a specific module.
-
-        Args:
-            module (nn.Module): A PyTorch module.
-            value (bool): Value to set for the gradient checkpointing attribute.
-        """
-        if isinstance(module, BertEncoder):
-            module.gradient_checkpointing = value
 
 
 class BertModel(BertPreTrainedModel):
