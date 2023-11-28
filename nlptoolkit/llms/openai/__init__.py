9f5d12029acaf4fda195b833a02c50a10ebb4bba
jianzhnie <jianzhnie@126.com>
2023-11-28 19:35:08 +0800

    update llama

---
+++ nlptoolkit/llms/openai/__init__.py
@@ -0,0 +1,4 @@
+from .config_openai import OpenAIGPTConfig
+from .modeling_openai import OpenAIGPTLMHeadModel, OpenAIGPTModel
+
+__all__ = ['OpenAIGPTConfig', 'OpenAIGPTLMHeadModel', 'OpenAIGPTModel']
