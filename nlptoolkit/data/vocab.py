d52d6d90d450791e8ba6da20eedfd04c67b7195c
jianzhnie <jianzhnie@126.com>
2024-11-14 17:52:34 +0800

    Update vocab.py

--- nlptoolkit/data/vocab.py
+++ nlptoolkit/data/vocab.py
@@ -1,17 +1,15 @@
-'''
-Author: jianzhnie
-Date: 2021-11-30 10:09:54
-LastEditTime: 2022-01-04 17:39:48
-LastEditors: jianzhnie
-Description:
+"""A comprehensive vocabulary management system for NLP tasks.
 
-'''
+This module provides a Vocab class that handles token-to-index mapping, special
+tokens, and various utilities for text processing tasks.
+"""
+
 import collections
 import io
 import json
 import os
-from collections import OrderedDict
-from typing import Dict, Iterable, List
+from collections import Counter, OrderedDict
+from typing import Any, Dict, Iterable, List, Optional, Union
 
 UNK_TOKEN = '<UNK>'
 SEP_TOKEN = '<SEP>'
@@ -24,25 +22,36 @@ BOW_TOKEN = '<BOW>'
 EOW_TOKEN = '<EOW>'
 
 
-def tokenize(lines, token='word'):
+def tokenize(lines: List[str], token: str = "word") -> List[List[str]]:
     """Split text lines into word or character tokens.
 
-    Defined in :numref:`sec_utils`
+    Args:
+        lines: List of input text lines to tokenize
+        token: Tokenization type, either 'word' or 'char'
+
+    Returns:
+        List of tokenized sequences
+
+    Raises:
+        AssertionError: If token type is invalid
     """
-    assert token in ('word', 'char'), 'Unknown token type: ' + token
+    assert token in ("word", "char"), f"Unknown token type: {token}"
     return [line.split() if token == 'word' else list(line) for line in lines]
 
 
-def count_corpus(tokens):
-    """统计词元的频率。
+def count_corpus(tokens: Union[List[str], List[List[str]]]) -> Counter:
+    """Count token frequencies in a corpus.
 
-    Defined in :numref:`sec_text_preprocessing`
+    Args:
+        tokens: Input tokens, either as a flat list or list of sequences
+
+    Returns:
+        Counter object with token frequencies
     """
-    # 这里的 `tokens` 是 1D 列表或 2D 列表
     if len(tokens) == 0 or isinstance(tokens[0], list):
-        # 将词元列表展平成使用词元填充的一个列表
+        # Flatten 2D list of tokens into 1D
         tokens = [token for line in tokens for token in line]
-    return collections.Counter(tokens)
+    return Counter(tokens)
 
 
 class Vocab(object):
@@ -78,17 +87,18 @@ class Vocab(object):
             of the vocabulary and associated with an index.
     """
 
-    def __init__(self,
-                 counter=None,
-                 max_size: int = None,
-                 min_freq: int = 1,
-                 token_to_idx: Dict[str, int] = None,
-                 unk_token: str = UNK_TOKEN,
-                 pad_token: str = PAD_TOKEN,
-                 bos_token: str = BOS_TOKEN,
-                 eos_token: str = EOS_TOKEN,
-                 **kwargs):
-
+    def __init__(
+        self,
+        counter: Optional[Counter] = None,
+        max_size: Optional[int] = None,
+        min_freq: int = 1,
+        token_to_idx: Optional[Dict[str, int]] = None,
+        unk_token: Optional[str] = UNK_TOKEN,
+        pad_token: Optional[str] = PAD_TOKEN,
+        bos_token: Optional[str] = BOS_TOKEN,
+        eos_token: Optional[str] = EOS_TOKEN,
+        **kwargs: Any,
+    ) -> None:
         self.unk_token = unk_token
         self.pad_token = pad_token
         self.bos_token = bos_token
@@ -102,16 +112,18 @@ class Vocab(object):
             'eos_token': eos_token,
         })
         special_token_dict.update(kwargs)
+
         # Check if special tokens are in kwargs
         special_tokens: List[str] = []
         self.special_token_dict = OrderedDict()
-        for (token_name, special_token) in special_token_dict.items():
+        for token_name, special_token in special_token_dict.items():
             # Test if kwarg specifies a special token
-            if not token_name.endswith('_token'):
+            if not token_name.endswith("_token"):
                 raise ValueError(
-                    '{} is invalid. Only keyword arguments '
+                    f"{token_name} is invalid. Only keyword arguments "
                     "that end in '_token' are supported "
-                    'to declare special tokens.'.format(token_name))
+                    "to declare special tokens."
+                )
 
             if special_token is not None and special_token not in special_tokens:
                 special_tokens.append(special_token)
@@ -119,53 +131,72 @@ class Vocab(object):
 
         # Map tokens to indices and indices to tokens
         if counter is None:
-            assert token_to_idx is not None, 'token_to_idx must be provided if counter is None'
+            assert (
+                token_to_idx is not None
+            ), "token_to_idx must be provided if counter is None"
             self.token_to_idx: Dict[str, int] = token_to_idx
             self.idx_to_token: Dict[int, str] = {
-                idx: token
-                for token, idx in token_to_idx.items()
+                idx: token for token, idx in token_to_idx.items()
             }
             self.add_tokens(special_tokens)
 
         else:
             # Map special tokens to index
-            self.idx_to_token = {
-                idx: special_token
-                for idx, special_token in enumerate(special_tokens)
-            }
-            self.token_to_idx = collections.OrderedDict()
+            self.idx_to_token = {idx: token for idx, token in enumerate(special_tokens)}
+            self.token_to_idx = OrderedDict()
             self.token_to_idx.update(
-                (token, idx) for idx, token in self.idx_to_token.items())
+                (token, idx) for idx, token in self.idx_to_token.items()
+            )
 
-            self._index_counter_keys(counter, special_tokens, max_size,
-                                     min_freq)
+            self._index_counter_keys(counter, special_tokens, max_size, min_freq)
 
-    def _index_counter_keys(self, counter, special_tokens, max_size, min_freq):
+    def _index_counter_keys(
+        self,
+        counter: Counter,
+        special_tokens: List[str],
+        max_size: Optional[int],
+        min_freq: int,
+    ) -> None:
+        """Index tokens from counter while respecting frequency and size
+        constraints.
+
+        Args:
+            counter: Token counter
+            special_tokens: List of special tokens to exclude from indexing
+            max_size: Maximum vocabulary size
+            min_freq: Minimum frequency threshold
+        """
         # Sort by frequency, then alphabetically
-        self.token_freqs = sorted(counter.items(),
-                                  key=lambda x: x[1],
-                                  reverse=True)
+        self.token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
         self.word_counter = {key: val for key, val in self.token_freqs}
         # Frequencies of special tokens are not counted when building vocabulary
         # in frequency order
+
         special_tokens = set(special_tokens)
         max_size = None if max_size is None else max_size + len(special_tokens)
+
         for token, freq in self.token_freqs:
             if freq < min_freq or len(self.idx_to_token) == max_size:
                 break
             if token not in special_tokens:
                 self.add_tokens(token)
 
-    def add_tokens(self, tokens):
+    def add_tokens(self, tokens: Union[str, List[str]]) -> None:
+        """Add new tokens to the vocabulary.
+
+        Args:
+            tokens: Single token or list of tokens to add
+        """
         if not isinstance(tokens, (list, tuple)):
             if tokens not in self.token_to_idx:
-                self.idx_to_token[len(self.idx_to_token)] = tokens
-                self.token_to_idx[tokens] = len(self.token_to_idx)
+                idx = len(self.idx_to_token)
+                self.idx_to_token[idx] = tokens
+                self.token_to_idx[tokens] = idx
         else:
             for token in tokens:
                 self.add_tokens(token)
 
-    def to_tokens(self, indices):
+    def to_tokens(self, indices: List[int]) -> Union[List[str], str]:
         """Maps the input indices to a token or list of tokens.
 
         Args:
@@ -177,11 +208,11 @@ class Vocab(object):
             will return a str. If `indices` is a list/tuple of integers, it will
             return a list of str.
         """
-        if hasattr(indices, '__len__') and len(indices) > 1:
+        if hasattr(indices, "__len__") and len(indices) > 1:
             return [self.idx_to_token[int(index)] for index in indices]
         return self.idx_to_token[indices]
 
-    def to_index(self, tokens):
+    def to_index(self, tokens: Union[str, List[str]]):
         """Maps the input tokens into indices.
 
         Args:
@@ -195,22 +226,48 @@ class Vocab(object):
         """
         return self[tokens]
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.idx_to_token)
 
-    def size(self):
+    def size(self) -> int:
         return len(self.idx_to_token)
 
     def __getitem__(self, tokens):
         if not isinstance(tokens, (list, tuple)):
-            return self.token_to_idx.get(tokens,
-                                         self.token_to_idx.get(self.unk_token))
+            return self.token_to_idx.get(tokens, self.token_to_idx.get(self.unk_token))
         return [self.__getitem__(token) for token in tokens]
 
-    def __contains__(self, token):
+    def __contains__(self, token: str) -> bool:
         return token in self.token_to_idx
 
-    def to_json(self, path=None):
+    def get_bos_token_id(self) -> Optional[int]:
+        """Get the index of the beginning-of-sequence token."""
+        return (
+            self.token_to_idx.get(self.bos_token)
+            if self.bos_token is not None
+            else None
+        )
+
+    def get_eos_token_id(self) -> Optional[int]:
+        """Get the index of the end-of-sequence token."""
+        return (
+            self.token_to_idx.get(self.eos_token)
+            if self.eos_token is not None
+            else None
+        )
+
+    def get_pad_token_id(self) -> Optional[int]:
+        """Get the index of the padding token."""
+        return (
+            self.token_to_idx.get(self.pad_token)
+            if self.pad_token is not None
+            else None
+        )
+
+    def get_token_freq(self):
+        return self.word_counter
+
+    def to_json(self, path: str = None):
         """Summarizes some information of the vocab as a JSON string. If a path
         is.
 
@@ -225,18 +282,18 @@ class Vocab(object):
             str: The JSON string including information of vocab.
         """
         vocab_dict = {}
-        vocab_dict['idx_to_token'] = dict(self.idx_to_token)
-        vocab_dict['token_to_idx'] = dict(self.token_to_idx)
-        vocab_dict['special_token'] = self.special_token_dict
-        vocab_dict['token_freqs'] = self.token_freqs
+        vocab_dict["idx_to_token"] = dict(self.idx_to_token)
+        vocab_dict["token_to_idx"] = dict(self.token_to_idx)
+        vocab_dict["special_token"] = self.special_token_dict
+        vocab_dict["token_freqs"] = self.token_freqs
         json_str = json.dumps(vocab_dict, indent=2, ensure_ascii=False)
         if path:
-            with io.open(path, 'w', encoding='utf-8') as f:
+            with io.open(path, "w", encoding="utf-8") as f:
                 f.write(json_str)
         return json_str
 
     @classmethod
-    def from_json(cls, json_str):
+    def from_json(cls, json_str: str):
         """Loads a `Vocab` from a JSON string or JSON file, which is generated
         by calling the `to_json` method.
 
@@ -247,23 +304,25 @@ class Vocab(object):
             Vocab: An instance of `Vocab` generated from the information contained in the JSON string.
         """
         if os.path.isfile(json_str):
-            with io.open(json_str, 'r', encoding='utf-8') as f:
+            with io.open(json_str, "r", encoding="utf-8") as f:
                 vocab_dict = json.load(f)
         else:
             vocab_dict = json.loads(json_str)
-        token_to_idx = vocab_dict.get('token_to_idx')
-        special_tokens = vocab_dict.get('special_token', dict())
+        token_to_idx = vocab_dict.get("token_to_idx")
+        special_tokens = vocab_dict.get("special_token", dict())
         vocab = cls(counter=None, token_to_idx=token_to_idx, **special_tokens)
         return vocab
 
     @classmethod
-    def from_dict(cls,
-                  token_to_idx,
-                  unk_token=None,
-                  pad_token=None,
-                  bos_token=None,
-                  eos_token=None,
-                  **kwargs):
+    def from_dict(
+        cls,
+        token_to_idx: Dict[str, int],
+        unk_token: Optional[str] = None,
+        pad_token: Optional[str] = None,
+        bos_token: Optional[str] = None,
+        eos_token: Optional[str] = None,
+        **kwargs: Any,
+    ):
         """Builds the `Vocab` from a dictionary.
 
         Args:
@@ -294,15 +353,17 @@ class Vocab(object):
         return vocab
 
     @staticmethod
-    def build_vocab(iterator: Iterable,
-                    max_size=None,
-                    min_freq=1,
-                    token_to_idx=None,
-                    unk_token=None,
-                    pad_token=None,
-                    bos_token=None,
-                    eos_token=None,
-                    **kwargs):
+    def build_vocab(
+        iterator: Iterable,
+        max_size: int = None,
+        min_freq: int = 1,
+        token_to_idx: Dict[str, int] = None,
+        unk_token: Optional[str] = None,
+        pad_token: Optional[str] = None,
+        bos_token: Optional[str] = None,
+        eos_token: Optional[str] = None,
+        **kwargs: Any,
+    ):
         """Builds the `Vocab` according to a given iterator and other
         information. It first iterates over the `iterator` to construct a
         `collections.Counter` and uses it to initialize the `Vocab`.
@@ -345,20 +406,22 @@ class Vocab(object):
         )
         return vocab
 
-    def save_vocabulary(self, path):
+    def save_vocabulary(self, path: str):
         """Save the vocabulary to a file."""
-        with open(path, 'w') as writer:
+        with open(path, "w") as writer:
             for idx in range(len(self.idx_to_token)):
-                writer.write(self.idx_to_token[idx] + '\n')
+                writer.write(self.idx_to_token[idx] + "\n")
 
     @classmethod
-    def load_vocabulary(cls,
-                        vocab_file,
-                        unk_token=None,
-                        pad_token=None,
-                        bos_token=None,
-                        eos_token=None,
-                        **kwargs):
+    def load_vocabulary(
+        cls,
+        vocab_file: str,
+        unk_token: Optional[str] = None,
+        pad_token: Optional[str] = None,
+        bos_token: Optional[str] = None,
+        eos_token: Optional[str] = None,
+        **kwargs,
+    ):
         """Builds the `Vocab` from a file, preserving all tokens by calling the
         `from_dict` method. The file contains a token per line, and the line
         index would be the index of the corresponding token.
@@ -382,36 +445,19 @@ class Vocab(object):
 
         token_lst = []
         vocab = collections.OrderedDict()
-        with open(vocab_file, 'r', encoding='utf-8') as reader:
+        with open(vocab_file, "r", encoding="utf-8") as reader:
             tokens = reader.readlines()
             for index, token in enumerate(tokens):
-                token = token.rstrip('\n').split('\t')[0]
+                token = token.rstrip("\n").split("\t")[0]
                 vocab[token] = index
         token_lst = list(vocab.keys())
         token_to_idx = {token: idx for idx, token in enumerate(token_lst)}
-        vocab = cls.from_dict(token_to_idx,
-                              unk_token=unk_token,
-                              pad_token=pad_token,
-                              bos_token=bos_token,
-                              eos_token=eos_token,
-                              **kwargs)
+        vocab = cls.from_dict(
+            token_to_idx,
+            unk_token=unk_token,
+            pad_token=pad_token,
+            bos_token=bos_token,
+            eos_token=eos_token,
+            **kwargs,
+        )
         return vocab
-
-    def get_unk_token_id(self):
-        return self.token_to_idx.get(
-            self.unk_token) if self.unk_token is not None else None
-
-    def get_bos_token_id(self):
-        return self.token_to_idx.get(
-            self.bos_token) if self.bos_token is not None else None
-
-    def get_eos_token_id(self):
-        return self.token_to_idx.get(
-            self.eos_token) if self.eos_token is not None else None
-
-    def get_pad_token_id(self):
-        return self.token_to_idx.get(
-            self.pad_token) if self.pad_token is not None else None
-
-    def get_token_freq(self):
-        return self.word_counter
